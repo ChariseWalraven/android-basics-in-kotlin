3cc34db3df8cd3cc61447b4a2dce942e4ebd169d
Charise Walraven <cherri.walraven@gmail.com>
2022-05-15 13:34:33 +0200

    whoops, here's the import statement

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 /**
  * This activity allows the user to roll the dice and view the result
@@ -23,20 +23,31 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         // create new dice with six sides for first dice and roll it for a random number
-        val firstDice = Dice(6)
-        val firstDiceRoll = firstDice.roll()
-        // update the screen with the result for the second dice (cast to a String)
-        val firstResultTV: TextView = findViewById(R.id.firstResult)
-        firstResultTV.text = firstDiceRoll.toString()
-        // create new dice with six sides for second dice and roll it for a random number
         val dice = Dice(6)
-        val secondDiceRoll = dice.roll()
-        // update the screen with the result for the second dice (cast to a String)
-        val secondResultTV: TextView = findViewById(R.id.secondResult)
-        secondResultTV.text = secondDiceRoll.toString()
+        val diceRoll = dice.roll()
+        // get dice image to change later
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+
+        // return the correct resource for the dice roll value
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // set image to correct resource
+        diceImage.setImageResource(drawableResource)
+
+        // set content description so screen readers have the right thing to read
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
+
+// Dice class that rolls. Pretty obvious, I guess.
 class Dice(private val sides: Int) {
 
     fun roll(): Int {
